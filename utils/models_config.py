e34ff3a4c2c1927bace03412063774bda6e83b50
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2025-07-02 14:13:07 +0100

    create hpo, 100 trials, fixes

--- utils/models_config.py
+++ utils/models_config.py
@@ -122,6 +122,7 @@ class ModelsConfig:
                     'AutoDeepNPTS',
                     'AutoDeepAR',
                     'AutoLSTM',
+                    'AutoKAN',
                     'AutoDilatedRNN',
                     'AutoTCN']
 
