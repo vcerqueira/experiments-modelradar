5496a7b727c114a999a630db5ff4f4f68492b972
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2025-07-07 12:40:32 +0100

    robustness analysis impl

--- scripts/experiments/run/nf_robustness.py
+++ scripts/experiments/run/nf_robustness.py
@@ -1,19 +1,9 @@
-# todo when i get some results
-
-
-SEEDS = [2025, 2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016]
-
-# get best params
-
-# for each seed, model
-# - run model
-# - evaluate
-
-# average results + standard deviation
-
 import warnings
 
+import pandas as pd
 from neuralforecast import NeuralForecast
+from utilsforecast.losses import smape
+from modelradar.evaluate.radar import ModelRadar
 
 from utils.load_data.config import DATASETS, DATA_GROUPS
 from utils.models_config import ModelsConfig
@@ -22,27 +12,56 @@ warnings.filterwarnings('ignore')
 
 # os.environ['PYTORCH_ENABLE_MPS_FALLBACK'] = '1'
 # ---- data loading and partitioning
-GROUP_IDX = 1
-EXPERIMENT = 'nf'
+SEEDS = [2025, 2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016]
+GROUP_IDX = 0
+EXPERIMENT = 'robust-nf'
 data_name, group = DATA_GROUPS[GROUP_IDX]
 print(data_name, group)
 data_loader = DATASETS[data_name]
 
+best_configs = pd.read_csv(f'assets/results/{data_name},{group},hpo-nf.csv').set_index('parameter')
 df, horizon, n_lags, freq_str, freq_int = data_loader.load_everything(group)
 # df, horizon, n_lags, freq_str, freq_int = data_loader.load_everything(group, sample_n_uid=5)
 # df = data_loader.get_uid_tails(df, tail_size=100)
 # df = data_loader.dummify_series(df)
 
+
 train, test = data_loader.train_test_split(df, horizon=horizon)
 
-# ---- model setup
-nf = NeuralForecast(models=ModelsConfig.get_auto_nf_models(horizon=horizon), freq=freq_str)
+results_list = []
+for seed_ in SEEDS:
+    # ---- model setup
+    nf = NeuralForecast(models=ModelsConfig.get_nf_models_config(horizon=horizon,
+                                                                 best_configs=best_configs,
+                                                                 random_seed=seed_),
+                        freq=freq_str)
 
-# ---- model fitting
-nf.fit(df=train)
+    # ---- model fitting
+    nf.fit(df=train)
 
-# ---- forecasts
-fcst_nf = nf.predict()
-fcst_nf = fcst_nf.reset_index().merge(test, on=['ds', 'unique_id'], how='right')
+    # ---- forecasts
+    fcst_nf = nf.predict()
+    fcst_nf = fcst_nf.merge(test, on=['ds', 'unique_id'], how='right')
+    fcst_nf['seed'] = seed_
 
-fcst_nf.to_csv(f'assets/results/{data_name},{group},{EXPERIMENT}.csv', index=False)
+    results_list.append(fcst_nf)
+
+results_df = pd.concat(results_list)
+
+metadata = ['unique_id', 'ds', 'y',
+            'trend_str', 'seas_str',
+            'is_anomaly', 'large_obs',
+            'large_uids', 'anomaly_status', 'Frequency', 'seed']
+model_names = results_df.columns[~results_df.columns.str.contains('|'.join(metadata))].tolist()
+
+radar = ModelRadar(cv_df=results_df,
+                   metrics=[smape],
+                   model_names=model_names,
+                   hardness_reference='AutoMLP',
+                   ratios_reference='AutoMLP',
+                   rope=10)
+
+err = radar.evaluate_by_group(group_col='seed', cv=results_df)
+print(err)
+
+err.to_csv(f'assets/results/{data_name},{group},{EXPERIMENT}.csv', index=False)
--- utils/models_config.py
+++ utils/models_config.py
@@ -1,3 +1,7 @@
+from typing import Dict, Optional
+from ast import literal_eval
+
+import pandas as pd
 from neuralforecast.auto import (AutoGRU,
                                  AutoKAN,
                                  AutoMLP,
@@ -10,6 +14,19 @@ from neuralforecast.auto import (AutoGRU,
                                  AutoDeepAR,
                                  AutoTCN,
                                  AutoDilatedRNN)
+
+from neuralforecast.models import (GRU,
+                                   KAN,
+                                   MLP,
+                                   LSTM,
+                                   DLinear,
+                                   NHITS,
+                                   PatchTST,
+                                   TFT,
+                                   DeepNPTS,
+                                   DeepAR,
+                                   TCN,
+                                   DilatedRNN)
 
 import lightgbm as lgb
 import xgboost as xgb
@@ -32,6 +49,42 @@ from statsforecast.models import (
     CrostonOptimized,
     WindowAverage
 )
+
+CONFIG_TYPES = {
+    'h': int,
+    'grid_size': int,
+    'spline_order': int,
+    'hidden_size': int,
+    'learning_rate': float,
+    'scaler_type': str,
+    'max_steps': int,
+    'batch_size': int,
+    'windows_batch_size': int,
+    'random_seed': int,
+    'accelerator': str,
+    'input_size': int,
+    'step_size': int,
+    'num_layers': int,
+    'moving_avg_window': int,
+    'n_pool_kernel_size': 'literal_eval',
+    'n_freq_downsample': 'literal_eval',
+    'dropout': float,
+    'n_layers': int,
+    'n_head': int,
+    'n_heads': int,
+    'patch_len': int,
+    'revin': bool,
+    'encoder_hidden_size': int,
+    'encoder_n_layers': int,
+    'context_size': int,
+    'decoder_hidden_size': int,
+    'inference_input_size': int,
+    'lstm_hidden_size': int,
+    'lstm_n_layers': int,
+    'lstm_dropout': float,
+    'cell_type': str,
+    'dilations': 'literal_eval'
+}
 
 COLORS = {
     'A': '#8B0000',  # Deep red
@@ -209,6 +262,104 @@ class ModelsConfig:
                 h=horizon,
                 num_samples=cls.N_SAMPLES,
                 alias=mod_name,
+            )
+
+            models.append(model_instance)
+
+        return models
+
+    @staticmethod
+    def convert_config_types(d: dict) -> dict:
+        """Convert dictionary values based on CONFIG_TYPES mapping.
+
+        Args:
+            d: Dictionary with keys matching CONFIG_TYPES
+
+        Returns:
+            Dictionary with values converted to specified types
+
+        Raises:
+            KeyError: If d contains keys not in CONFIG_TYPES
+        """
+        unknown_keys = set(d.keys()) - set(CONFIG_TYPES.keys())
+        if unknown_keys:
+            raise KeyError(f"Unknown config keys: {unknown_keys}")
+
+        converted = {}
+        for k, v in d.items():
+            type_converter = CONFIG_TYPES[k]
+            if type_converter == 'literal_eval':
+                converted[k] = literal_eval(v)
+            else:
+                converted[k] = type_converter(v)
+        return converted
+
+    @classmethod
+    def get_nf_models_config(
+            cls,
+            horizon: int,
+            limit_val_batches: bool = False,
+            best_configs: Optional[pd.DataFrame] = None,
+            random_seed: Optional[int] = None):
+
+        NEED_CPU = ['GRU',
+                    'DeepNPTS',
+                    'DeepAR',
+                    'LSTM',
+                    'KAN',
+                    'DilatedRNN',
+                    'TCN']
+
+        model_cls = {
+            'KAN': KAN,
+            'MLP': MLP,
+            'DLinear': DLinear,
+            'NHITS': NHITS,
+            'DeepNPTS': DeepNPTS,
+            'TFT': TFT,
+            'PatchTST': PatchTST,
+            'GRU': GRU,
+            'DeepAR': DeepAR,
+            'LSTM': LSTM,
+            'DilatedRNN': DilatedRNN,
+            'TCN': TCN,
+        }
+
+        models = []
+        for mod_name, mod in model_cls.items():
+            # mod_name = 'AutoKAN'
+            # mod_name = 'KAN'
+            conf = best_configs[f'Auto{mod_name}'].copy()
+            conf['max_steps'] = int(float(conf['max_steps']))
+            conf.pop('loss')
+            conf.pop('valid_loss')
+            conf.pop('h')
+
+            if pd.isna(conf['scaler_type']):
+                conf['scaler_type'] = 'identity'
+
+            conf = conf.dropna().to_dict()
+
+            conf = cls.convert_config_types(conf)
+            print(conf)
+
+            if random_seed is not None:
+                conf['random_seed'] = random_seed
+
+            if mod_name in NEED_CPU:
+                # for RNN's
+                conf['accelerator'] = 'cpu'
+            else:
+                conf['accelerator'] = 'mps'
+
+            if limit_val_batches:
+                # for M4
+                conf['limit_val_batches'] = 50
+
+            model_instance = mod(
+                h=horizon,
+                alias=f'Auto{mod_name}',
+                **conf
             )
 
             models.append(model_instance)
